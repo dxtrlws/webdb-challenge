f84dd5818f0813deb681134d52ed440b16748fb6
Dexter Lewis <dexter.lewis@gmail.com>
2019-06-02 21:36:28 -0400

    Add project route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const server = express()
+const projectRouter = require('./routes/projectRoutes')
 
 server.use(express.json())
+server.use('/api/projects', projectRouter)
 
 server.get('/', (req, res) => {
     res.send('server running')
