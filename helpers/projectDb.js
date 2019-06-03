1f37321f88943e3dfc3380dbc060fcf412385ea3
Dexter Lewis <dexter.lewis@gmail.com>
2019-06-02 21:28:06 -0400

    add helper to query projects

---
+++ helpers/projectDb.js
@@ -0,0 +1,29 @@
+const db = require('../data/dbConfig')
+
+module.exports = {
+    get,
+    findById,
+    insert,
+    remove
+}
+
+function get() {
+    return db('project')
+}
+
+function findById(id) {
+    return db('project')
+    .where({id})
+    .first()
+}
+
+function insert(project) {
+    return db('project')
+    .insert(project)
+}
+
+function remove(id) {
+    return db('project')
+    .where({id})
+    .del()
+}
